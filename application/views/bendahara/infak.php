50e23535f414b9ebd0ac0a144bcfc9d7a4212fd8
Andi Sujati Rahman <andyavengedsevenfold@gmail.com>
2021-11-28 00:35:25 +0700

    menambahkan kolom bukti dan aksi serta menambhakan total informasi keuangan

--- application/views/bendahara/infak.php
+++ application/views/bendahara/infak.php
@@ -6,34 +6,45 @@
             <a class="btn btn-primary mb-3"><i class="fas fa-minus-square"></i> Pengeluaran</a>
             <a class="btn btn-primary mb-3"><i class="fas fa-hand-holding-usd"></i> Pengajuan</a>
             <a class="btn btn-info mb-3"><i class="fas fa-print"></i> Cetak</a>
-
-            <div class="mr-lg-auto">
-            </div>
         </div>
-
-        <table class="table table-bordered table-hover table-striped" id="dataTable" width="100%">
-            <caption class="bi bi-list-check caption-top"> Daftar donatur Infaq Masjid Jami Assalam</caption>
-            <thead>
+        <table class="table table-bordered table-striped" id="dataTable" style="width: 100%;">
+            <thead class="text-center">
                 <tr>
                     <th>No</th>
                     <th>Nama</th>
                     <th>Nominal</th>
                     <th>Keterangan</th>
                     <th>Tanggal</th>
+                    <th>Bukti</th>
+                    <th>Aksi</th>
                 </tr>
+            </thead>
             <tbody>
                 <?php $i = 1;
                 foreach ($infaq as $ifq) : ?>
                     <tr>
-                        <td><?= $i++; ?></td>
+                        <td class="text-center"><?= $i++; ?></td>
                         <td><?= $ifq['nama']; ?></td>
                         <td>Rp. <?= number_format($ifq['nominal']); ?></td>
                         <td><?= $ifq['keterangan']; ?></td>
-                        <td><?= $ifq['tanggal']; ?></td>
+                        <td class="text-center"><?= $ifq['tanggal']; ?></td>
+                        <td class="text-center"><?= $ifq['bukti']; ?></td>
+                        <td class="text-center">
+                            <a href="#" class="text-danger"><i class="fas fa-trash-alt"></i></a>
+                            <a href="#" class="text-success"><i class="fas fa-edit"></i></a>
+                            <a href="#" class="text-primary"><i class="fas fa-eye"></i></a>
+                        </td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
-            </thead>
         </table>
+
+        <div class="card text-white bg-info mt-3 mb-3" style="max-width: 50%;">
+            <div class="card-header bg-dark"><strong>Total Keuangan Masjid Jami Assalam</strong></div>
+            <div class="card-body">
+                <h1 class="card-title">Rp. 50.000.000,- </h1>
+                <p class="card-text">Total keuangan adalah jumlah dari seluruh pemasukan dan pengeluaran, yang dijumlahkan secara realtime leh sistem</p>
+            </div>
+        </div>
     </div>
 </div>
